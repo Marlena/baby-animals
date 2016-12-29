a255836552c3237f89b2d54a8b9fdd0e74ee69c5
Marlena Compton <marlena.compton@gmail.com>
2016-12-28 16:30:50 -0800

    added a procfile

--- tasks/server.js
+++ tasks/server.js
@@ -1,9 +1,35 @@
 const gulp = require('gulp');
-var gls = require('gulp-live-server');
+var connect = require('gulp-connect');
+const http = require('http');
+const serveStatic = require('serve-static');
+const nodemon = require('gulp-nodemon');
 
-const server = gls.static();
+//server example code
+//https://github.com/micahblu/gulp-connect-php/issues/2
+
+let port;
+
+if (process.env.NODE_ENV == 'production') {
+   port = process.env.PORT;
+}
+else{
+   port = 3000;
+}
 
 
 gulp.task('server', function(){
-   server.start()
+
+   var app = require('connect')()
+       .use(serveStatic('./public'));
+
+
+   require('http').createServer(app)
+       .listen(port);
+
+ });
+
+gulp.task('nodemon', function(){
+    nodemon({
+        script: 'app.js'
+    })
 });
